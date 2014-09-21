70af03eeaa75b0b6bbc269e3b1fdb7ec9fcd2860
U-thundercat\daniel <daniel@thundercat.(none)>
2014-09-21 21:21:40 +0200

    init

---
+++ AppLibrary/src/main/java/de/derschimi/applibrary/AppFragment.java
@@ -0,0 +1,32 @@
+package de.derschimi.applibrary;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.*;
+import android.widget.TextView;
+
+/**
+ * Created by daniel on 21.09.2014.
+ */
+public class AppFragment extends Fragment {
+
+
+
+    private String name;
+    public AppFragment(String name){
+        this.name = name;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        View v = inflater.inflate(R.layout.fragment, container, false);
+
+        TextView tv = (TextView)v.findViewById(R.id.fragmentname);
+        tv.setText(name);
+        return v;
+    }
+
+
+}
---
+++ AppTests/src/test/groovy/de/derschimi/app/MainActivitySpecification.groovy
@@ -0,0 +1,27 @@
+package de.derschimi.app
+
+import android.widget.TextView
+import org.robolectric.Robolectric
+import org.robolectric.annotation.Config
+import pl.polidea.robospock.RoboSpecification
+
+/**
+ * Created by daniel on 21.09.2014.
+ */
+@Config(manifest = "/App/src/main/AndroidManifest.xml")
+class MainActivitySpecification extends RoboSpecification {
+
+    def "Should say hello world"() {
+        setup:
+        def mainActivity = Robolectric.buildActivity(AppActivity.class).create().get()
+
+        when:
+        def tv = (TextView) mainActivity.findViewById(R.id.fragmentname)
+
+        then:
+        "Account{name='JohnWhite', password='24a28dbe'}" == tv.getText()
+    }
+  
+
+
+}
