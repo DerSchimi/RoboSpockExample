cf264acfe98e41386716c68de108ccc1d2a94aa4
Aleksander Piotrowski <pelotasplus@gmail.com>
2014-09-24 12:53:54 +0200

    fix AppFragment -- add missing imports and support-v4 dependency to build.gradle as this fragment uses fragments from support library (android.support.v4.app.Fragment vs android.app.Fragment)

--- AppLibrary/src/main/java/de/derschimi/applibrary/AppFragment.java
+++ AppLibrary/src/main/java/de/derschimi/applibrary/AppFragment.java
@@ -2,6 +2,9 @@ package de.derschimi.applibrary;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 /**
